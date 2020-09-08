a4062092dbc98a36a6ed363ef083665a1ab7e6be
Ajak Cyer <ajakcyer97@gmail.com>
2020-09-08 00:59:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -226,15 +226,14 @@ def big_shoe_rebounds
     
     player_array.each do |player_data|
       #binding.pry
-      binding.pry
+      #binding.pry
       if biggest_shoes < player_data[:shoe]
         biggest_shoes = player_data[:shoe]
         biggest_shoes_rebounds = player_data[:rebounds]
-        
       end
     end
     #binding.pry
-    biggest_shoes_rebounds
+    return biggest_shoes_rebounds
     biggest_shoes
   end
 
